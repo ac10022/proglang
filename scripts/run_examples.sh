6f790126853911107b39a5dd2a026051a2d0673f
Yehor Radchenko <u32integertype@protonmail.com>
2026-06-30 12:03:49 +0100

    feat: add arguments validation in build&run script

--- scripts/run_examples.sh
+++ scripts/run_examples.sh
@@ -3,6 +3,12 @@ SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 PROJECT_ROOT=$(dirname "$SCRIPT_DIR")
 cd "$PROJECT_ROOT"
 
+if [ $# -ne 1 ]; then 
+	echo "invalid arguments"
+	echo "usage: ./run_examples.sh <path to .proglang program>"
+	exit 1
+fi
+
 INPUT=$1
 ABS_FILE="$PROJECT_ROOT/$INPUT"
 
