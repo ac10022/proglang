a90d50228ba64de78648fb74e0c3454a222997de
Yehor Radchenko <u32integertype@protonmail.com>
2026-08-26 03:08:29 +0100

    create codegen context, add comments/notes

--- include/codegen.h
+++ include/codegen.h
@@ -3,6 +3,42 @@
 
 #include "optimiser.h"
 
+/*
+ * by the end of code generation, we have to end up with
+ * an assembly file (.s)
+ *
+ * let's take this for example:
+ * ----------------------------
+ *     int main() {
+ *         int x = 10;
+ *         return 0;
+ *     }
+ * ----------------------------
+ * our generator should spit out something like:
+ * ----------------------------
+ * main:
+ *   addi sp, sp, -32 # "grow" stack by 32 bytes (for example frame size is 32 bytes)
+ *   sd   ra, 24(sp)  # save return address into stack slot with offset 24 (last 8 bytes of frame)
+ *   sd   s0, 16(sp)  # save previous frame pointer into stack slot with offset 16 (second last 8 bytes of frame)
+ *   addi s0, sp, 32  # load current frame pointer into frame pointer register
+ * ----------------------------
+ * NOTE: s0 can also be referred to as fp (frame pointer)
+ */
+
+// when allocating slots, we create a one-to-one mappings
+// between variables or symbols (temps or t's as well) and
+// and the stack slots
+typedef struct {
+    IROperand operand;
+    int offset;
+
+} StackSlot;
+
+typedef struct{
+    FILE *out;
+
+} CodegenContext;
+
 // i think we will hold our horses for this one
 enum Target {
     X86_64,
@@ -10,6 +46,20 @@ enum Target {
 };
 
 char *create_asm_outpath(char *filepath);
+
+void initialise_codegen_context(CodegenContext *codegen_context, FILE *out);
 void generate_asm(IRInstruction* ir_instruction, CompilerContext* c_ctx);
+
+// creates new stack frame
+void emit_prologue(CodegenContext *codegen_context);
+
+// removes the stack frame
+void emit_epilogue(CodegenContext *codegen_context);
+
+void emit_load(CodegenContext *codegen_cotext);
+void emit_store(CodegenContext *codegen_context);
+
+void emit_add(CodegenContext *codegen_context);
+void emit_addi(CodegenContext *codegen_context);
 
 #endif //CODEGEN_H
--- src/codegen.c
+++ src/codegen.c
@@ -3,6 +3,9 @@
 /*
  * TODO: the function has to extract the name of .s file, i.e: main.proglang -> main.s
  *
+ * i dont know whether output name has to match outpath or filepath,
+ * but i think its irrelevant for now as at the end, assembly and object files
+ * will be most likely deleted after program compilation
  *
  */
 char *create_asm_outpath(char *filepath) {
@@ -11,8 +14,14 @@ char *create_asm_outpath(char *filepath) {
     return outpath;
 }
 
+void initialise_codegen_context(CodegenContext *codegen_context, FILE *out) {
+    codegen_context->out = out;
+
+}
+
 void generate_asm(IRInstruction *ir_instruction, CompilerContext *c_ctx) {
-    char *outpath = create_asm_outpath(c_ctx->filepath);
+    //char *outpath = create_asm_outpath(c_ctx->filepath);
+    char *outpath = "a.s";
     FILE *out = fopen(outpath, "w");
 
     if (!out) {
@@ -20,5 +29,11 @@ void generate_asm(IRInstruction *ir_instruction, CompilerContext *c_ctx) {
         return;
     }
 
+    CodegenContext codegen_context = {};
+    initialise_codegen_context(&codegen_context, out);
+
+    for (; ir_instruction->op != IR_HALT; ir_instruction = ir_instruction->next) {
+
+    }
 
 }
